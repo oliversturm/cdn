20bf7ce535a41d27cd9512cd4cd1fecc82b58f1a
Oli Sturm <oliver+commitsigning@oliversturm.com>
2024-11-01 22:15:13 +0000

    chore: add comment

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # prepare the environment
 export WASI_SDK_PATH=$(cat /usr/local/wasi-sdk-path)
 
+# figure out the command to run
 if [ "$1" = "wasmtime" ]; then
   CMD=/usr/local/bin/wasmtime
   shift
